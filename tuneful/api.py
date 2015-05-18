68b2a8e398ce24dba8459c63c73b68262ca24ea5
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-19 01:28:10 +0400

    api: create song_post() endpoint that uses POST method

--- tuneful/api.py
+++ tuneful/api.py
@@ -14,10 +14,8 @@ from utils import upload_path
 
 song_schema = {
     "properties": {
-        "file": {
-            "id": { "type": "int" },
-            "name": { "type": "string" }
-        }
+        "file": { "type": "object" },
+        "id": { "type": "number" }
     }
 }
 
@@ -28,3 +26,24 @@ def songs_get():
     songs = session.query(models.Song).all()
     data = json.dumps([song.as_dictionary() for song in songs])
     return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/songs", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def song_post():
+    """ Add a new song """
+    data = request.json
+
+    try:
+        validate(data, song_schema)
+    except ValidationError as error:
+        data = { "message": error.message }
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+    song = models.Song(file=models.File())
+    session.add(song)
+    session.commit()
+
+    data = json.dumps(song.as_dictionary())
+    headers = { "Location": url_for("songs_get") }
+    return Response(data, 201, headers=headers, mimetype="application/json")
