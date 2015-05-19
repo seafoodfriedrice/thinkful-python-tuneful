c1d7e25b54a8147a352b8964b4c1785d626afd35
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-19 23:43:20 +0400

    tests: add testSongPut() which tests PUT method on API

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -50,7 +50,7 @@ class TestAPI(unittest.TestCase):
         """ Post a new song """
         data = {
             "file": {
-                "id": 7
+                "name": "new_song.mp3"
             }
         }
         response = self.client.post("/api/songs",
@@ -63,3 +63,31 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(urlparse(response.headers.get("Location")).path,
                          "/api/songs")
 
+    def testSongPut(self):
+        """ Post a new song """
+        data = {
+            "file": {
+                "name": "new_song.mp3"
+            }
+        }
+        # Create song using POST which will create File.id = 1
+        response = self.client.post("/api/songs",
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=[("Accept", "application/json")]
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.mimetype, "application/json")
+        self.assertEqual(urlparse(response.headers.get("Location")).path,
+                         "/api/songs")
+        post_data = json.loads(response.data)
+        self.assertEqual(post_data["file"]["name"], "new_song.mp3")
+        # Change file name and use PUT to update to File.id = 1
+        data["file"]["name"] = "updated_song.mp3"
+        response = self.client.put("/api/songs/1",
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=[("Accept", "application/json")]
+        )
+        updated_data = json.loads(response.data)
+        self.assertEqual(updated_data["file"]["name"], "updated_song.mp3")
