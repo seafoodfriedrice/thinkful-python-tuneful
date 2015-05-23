1774817a0671903ca779422c24c35f4cf1bd74c1
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-23 21:39:25 +0400

    tests: add testGetUploadedFile and testFileUpload tests

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -120,3 +120,37 @@ class TestAPI(unittest.TestCase):
         delete_data = json.loads(response.data)
         self.assertEqual(delete_data["message"],
                          "File id 1 has been deleted.")
+
+    def testGetUploadedFile(self):
+        path = upload_path("test.txt")
+        with open(path, "w") as f:
+            f.write("File contents")
+
+        response = self.client.get("/uploads/test.txt")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "text/plain")
+        self.assertEqual(response.data, "File contents")
+
+    def testFileUpload(self):
+        data = {
+            "file": (StringIO("File contents"), "test.txt")
+        }
+
+        response = self.client.post("/api/files",
+            data=data,
+            content_type="multipart/form-data",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data= json.loads(response.data)
+        self.assertEqual(urlparse(data["path"]).path, "/uploads/test.txt")
+
+        path = upload_path("test.txt")
+        self.assertTrue(os.path.isfile(path))
+        with open(path) as f:
+            contents = f.read()
+        self.assertEqual(contents, "File contents")
