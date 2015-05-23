31f4443602e8d16bd19f9b90c5015180b880fb26
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-23 21:38:37 +0400

    api: add route functions to get uploaded files and upload files

--- tuneful/api.py
+++ tuneful/api.py
@@ -85,3 +85,25 @@ def song_delete(id):
     message = "File id {} has been deleted.".format(id)
     data = json.dumps({"message": message })
     return Response(data, 200, mimetype="application/json")
+
+@app.route("/uploads/<filename>", methods=["GET"])
+def uploaded_file(filename):
+    return send_from_directory(upload_path(), filename)
+
+@app.route("/api/files", methods=["POST"])
+@decorators.require("multipart/form-data")
+@decorators.accept("application/json")
+def file_post():
+    file = request.files.get("file")
+    if not file:
+        data = {"message": "Could not find file data"}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+    filename = secure_filename(file.filename)
+    db_file = models.File(filename=filename)
+    session.add(db_file)
+    session.commit()
+    file.save(upload_path(filename))
+
+    data = db_file.as_dictionary()
+    return Response(json.dumps(data), 201, mimetype="application/json")
